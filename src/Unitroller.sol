fd3186a559e93241cf3927db1e8c2339eaf60f84
gregrolwes <rolwesg@gmail.com>
2021-12-30 15:51:25 -0600

    Add compound interfaces

---
+++ src/Unitroller.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.7;
+
+/**
+ * @title ComptrollerCore
+ * @dev Storage for the comptroller is at this address, while execution is delegated to the `comptrollerImplementation`.
+ * CTokens should reference this contract as their comptroller.
+ */
+interface Unitroller {
+  function _setPendingImplementation(address newPendingImplementation)
+    external
+    returns (uint256);
+
+  function _setPendingAdmin(address newPendingAdmin) external returns (uint256);
+}
